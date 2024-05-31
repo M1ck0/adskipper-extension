7976baa4f2d45ede78550fec7fed225bbd80f566
miletadulovic <dulovicmileta1@gmail.com>
2024-05-31 22:15:17 +0200

    Switched to MutationObserver from setInterval

--- options/options.js
+++ options/options.js
@@ -1,12 +1,12 @@
 // Defining constants
 const WEBSITES = [
-  {domain: "https://youtube.com", name: "youtube", sidebarName: "YouTube"},
+  { domain: "https://youtube.com", name: "youtube", sidebarName: "YouTube" },
 ];
 
 const OPTIONS = {
-  "youtube": ["ads", "shorts", "recommendations", "errors"],
+  youtube: ["ads", "shorts", "recommendations", "errors"],
 };
-const SAVE_BUTTON_ID = 'save';
+const SAVE_BUTTON_ID = "save";
 
 // const DEFAULT_OPTIONS = {
 //   "youtube-ads": true,
@@ -21,21 +21,25 @@ const SAVE_BUTTON_ID = 'save';
 // Generic functions to operate on websites
 const resetWebsiteAppearance = (website) => {
   document.getElementById(`data-item-${website.name}`).style.display = "none";
-  document.getElementById(`link-item-${website.name}`).style.backgroundColor = "#fff";
-}
+  document.getElementById(`link-item-${website.name}`).style.backgroundColor =
+    "#fff";
+};
 
 const setActiveWebsite = (website) => {
-  document.getElementById(`link-item-${website.name}`).style.backgroundColor = "#eee";
+  document.getElementById(`link-item-${website.name}`).style.backgroundColor =
+    "#eee";
   document.getElementById(`data-item-${website.name}`).style.display = "flex";
-}
+};
 
 // Adding Event Listeners to the links
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   WEBSITES.forEach(function (website) {
-    document.getElementById(`link-item-${website.name}`).addEventListener('click', function () {
-      WEBSITES.forEach(resetWebsiteAppearance);
-      setActiveWebsite(website);
-    });
+    document
+      .getElementById(`link-item-${website.name}`)
+      .addEventListener("click", function () {
+        WEBSITES.forEach(resetWebsiteAppearance);
+        setActiveWebsite(website);
+      });
   });
 
   document.getElementById(`link-item-${WEBSITES[0].name}`).click();
@@ -44,14 +48,16 @@ document.addEventListener('DOMContentLoaded', function () {
 // Restore and Save functions for options
 const restoreOptions = () => {
   chrome.storage.sync.get((items) => {
-    WEBSITES.forEach(website => {
+    WEBSITES.forEach((website) => {
+      console.log(OPTIONS);
       OPTIONS[website.name].forEach((option) => {
         const storedValue = items[`${website.name}-${option}`];
 
         // By default, set `ads` option to true. Other options default to false.
-        const defaultValue = option === 'ads';
+        const defaultValue = option === "ads";
 
-        document.getElementById(`switch-${website.name}-${option}`).checked = (storedValue !== undefined) ? storedValue : defaultValue;
+        document.getElementById(`switch-${website.name}-${option}`).checked =
+          storedValue !== undefined ? storedValue : defaultValue;
       });
     });
   });
@@ -59,48 +65,63 @@ const restoreOptions = () => {
 
 const saveOptions = () => {
   let options = {};
-  WEBSITES.forEach(website => {
-    OPTIONS[website.name].forEach(option => {
-      options[`${website.name}-${option}`] = document.getElementById(`switch-${website.name}-${option}`).checked;
+  WEBSITES.forEach((website) => {
+    OPTIONS[website.name].forEach((option) => {
+      try {
+        options[`${website.name}-${option}`] = document.getElementById(
+          `switch-${website.name}-${option}`,
+        )?.checked;
+      } catch (e) {
+        console.log("err: ", e);
+      }
     });
   });
 
   chrome.storage.sync.set(options, function () {
     // Show message when item is saved
-    document.getElementById('status').innerHTML = 'Options saved successfully. Please reload your page for changes to take effect';
+    document.getElementById("status").innerHTML =
+      "Options saved successfully. Please reload your page for changes to take effect";
 
     setTimeout(() => {
-      document.getElementById('status').innerHTML = '';
-    }, 3000)
+      document.getElementById("status").innerHTML = "";
+    }, 3000);
   });
 };
 
 // Adding Event Listeners for save button and initial load
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   restoreOptions();
-  document.getElementById(SAVE_BUTTON_ID).addEventListener('click', saveOptions);
+  document
+    .getElementById(SAVE_BUTTON_ID)
+    .addEventListener("click", saveOptions);
   document.getElementById(`link-item-${WEBSITES[0].name}`).click();
 
-  chrome.storage.sync.get(["youtube-error-message", "youtube-errors"]).then(({
-         "youtube-error-message": youtubeErrorMessage,
-          "youtube-errors": youtubeErrors
-  }) => {
-    if (youtubeErrors) {
-      if (youtubeErrorMessage) {
-        const errorHTML = `
+  chrome.storage.sync
+    .get(["youtube-error-message", "youtube-errors"])
+    .then(
+      ({
+        "youtube-error-message": youtubeErrorMessage,
+        "youtube-errors": youtubeErrors,
+      }) => {
+        if (youtubeErrors) {
+          if (youtubeErrorMessage) {
+            const errorHTML = `
           <p>${youtubeErrorMessage.stack}</p>`;
 
-        document.getElementById('youtube-error').innerHTML = errorHTML;
-      }
-    } else {
-      document.getElementById('youtube-error').parentElement.style.display = 'none';
-    }
-  });
+            document.getElementById("youtube-error").innerHTML = errorHTML;
+          }
+        } else {
+          document.getElementById("youtube-error").parentElement.style.display =
+            "none";
+        }
+      },
+    );
 });
 
 // Use the Array.prototype.map method to create HTML for each website
-const websiteHtml = WEBSITES.map((website) =>
-  `<div class="link-item" id="link-item-${website.name}">
+const websiteHtml = WEBSITES.map(
+  (website) =>
+    `<div class="link-item" id="link-item-${website.name}">
     <div class="link-item-title">
       <img
         src="https://www.google.com/s2/favicons?domain=${website.domain}&sz=128"
@@ -110,8 +131,9 @@ const websiteHtml = WEBSITES.map((website) =>
       <h5>${website.sidebarName}</h5>
     </div>
     <p>Toggle ${website.sidebarName} features on or off</p>
-  </div>`
-).join('');
+  </div>`,
+).join("");
 
 // Add the generated HTML to your page
-document.querySelector('.links').innerHTML = websiteHtml + document.querySelector('.links').innerHTML;
+document.querySelector(".links").innerHTML =
+  websiteHtml + document.querySelector(".links").innerHTML;
--- scripts/youtube.js
+++ scripts/youtube.js
@@ -1,10 +1,134 @@
+// const defined = (v) => v !== null && v !== undefined;
+//
+// const clickSkipButton = (adElement) => {
+//   const skipButton = adElement.querySelector(".ytp-ad-skip-button-modern");
+//
+//   if (defined(skipButton)) {
+//     skipButton.click();
+//
+//     return true;
+//   }
+// };
+//
+// const stopYoutubeAd = () => {
+//   const adElement = document.querySelector(".ad-showing");
+//
+//   if (defined(adElement)) {
+//     const video = adElement?.querySelector("video");
+//
+//     if (defined(video)) {
+//       // const skipped = clickSkipButton(adElement);
+//       //
+//       // if (!skipped) {
+//       video.currentTime = 99999;
+//
+//       clickSkipButton(adElement);
+//
+//       // }
+//     }
+//   }
+//
+//   const overlayAds = document.querySelectorAll(".ytp-ad-overlay-slot");
+//
+//   for (const overlayAd of overlayAds) {
+//     overlayAd.style.visibility = "hidden";
+//   }
+// };
+//
+// const hideYoutubeShorts = () => {
+//   const shortsHomePage = document.querySelectorAll("[is-shorts]");
+//   const shortsHistoryPage = document.querySelectorAll(
+//     "ytd-reel-shelf-renderer",
+//   );
+//   const shortsSidebar = document.querySelector('[title="Shorts"]');
+//
+//   if (shortsHomePage?.length) {
+//     shortsHomePage.forEach((item) => item.remove());
+//   }
+//
+//   if (shortsHistoryPage?.length) {
+//     shortsHistoryPage.forEach((item) => item.remove());
+//   }
+//
+//   if (shortsSidebar) {
+//     shortsSidebar.remove();
+//   }
+// };
+//
+// const removeRecommendationsAtTheEnd = () => {
+//   const recommendations = document.querySelectorAll(
+//     ".ytd-player .ytp-ce-element",
+//   );
+//
+//   if (recommendations?.length) {
+//     recommendations.forEach((item) => item.remove());
+//   }
+// };
+//
+// chrome.storage.sync
+//   .get([
+//     "youtube-ads",
+//     "youtube-shorts",
+//     "youtube-errors",
+//     "youtube-recommendations",
+//   ])
+//   .then(
+//     ({
+//       "youtube-ads": youtubeAds,
+//       "youtube-shorts": youtubeShorts,
+//       "youtube-errors": youtubeErrors,
+//       "youtube-recommendations": youtubeRecommendations,
+//     }) => {
+//       setInterval(() => {
+//         if (youtubeAds === true || youtubeAds === undefined) {
+//           try {
+//             stopYoutubeAd();
+//           } catch (e) {
+//             if (youtubeErrors) {
+//               // Extract relevant information from the error object
+//               const errorInfo = {
+//                 stack: e.stack,
+//               };
+//
+//               // Save the error information to chrome storage
+//               chrome.storage.sync.set({ "youtube-error-message": errorInfo });
+//             }
+//           }
+//         }
+//
+//         if (youtubeShorts) {
+//           try {
+//             hideYoutubeShorts();
+//           } catch (e) {
+//             console.error(e);
+//           }
+//         }
+//
+//         if (youtubeRecommendations) {
+//           try {
+//             removeRecommendationsAtTheEnd();
+//           } catch (e) {
+//             console.error(e);
+//           }
+//         }
+//       }, 70);
+//     },
+//   );
+
 const defined = (v) => v !== null && v !== undefined;
 
-const clickSkipButton = (adElement) => {
-  const skipButton = adElement.querySelector(".ytp-ad-skip-button-modern");
+const clickSkipButton = () => {
+  const skipButton = document.querySelector(".ytp-skip-ad-button");
+  const skipButtonModern = document.querySelector(".ytp-ad-skip-button-modern");
 
   if (defined(skipButton)) {
     skipButton.click();
+
+    return true;
+  }
+
+  if (defined(skipButtonModern)) {
+    skipButtonModern.click();
 
     return true;
   }
@@ -14,22 +138,20 @@ const stopYoutubeAd = () => {
   const adElement = document.querySelector(".ad-showing");
 
   if (defined(adElement)) {
-    const skipped = clickSkipButton(adElement);
+    const video = adElement.querySelector("video");
 
-    if (!skipped) {
-      const video = adElement?.querySelector("video");
-
+    if (defined(video) && video.closest(".ad-interrupting")) {
       video.currentTime = video.duration;
-
-      clickSkipButton(adElement);
     }
   }
 
+  clickSkipButton();
+
   const overlayAds = document.querySelectorAll(".ytp-ad-overlay-slot");
 
-  for (const overlayAd of overlayAds) {
+  overlayAds.forEach((overlayAd) => {
     overlayAd.style.visibility = "hidden";
-  }
+  });
 };
 
 const hideYoutubeShorts = () => {
@@ -39,14 +161,8 @@ const hideYoutubeShorts = () => {
   );
   const shortsSidebar = document.querySelector('[title="Shorts"]');
 
-  if (shortsHomePage?.length) {
-    shortsHomePage.forEach((item) => item.remove());
-  }
-
-  if (shortsHistoryPage?.length) {
-    shortsHistoryPage.forEach((item) => item.remove());
-  }
-
+  shortsHomePage.forEach((item) => item.remove());
+  shortsHistoryPage.forEach((item) => item.remove());
   if (shortsSidebar) {
     shortsSidebar.remove();
   }
@@ -56,62 +172,81 @@ const removeRecommendationsAtTheEnd = () => {
   const recommendations = document.querySelectorAll(
     ".ytd-player .ytp-ce-element",
   );
+  recommendations.forEach((item) => item.remove());
+};
 
-  if (recommendations?.length) {
-    recommendations.forEach((item) => item.remove());
+// Function to start observing the DOM based on user settings
+const startObservers = (settings) => {
+  const {
+    "youtube-ads": youtubeAds,
+    "youtube-shorts": youtubeShorts,
+    "youtube-errors": youtubeErrors,
+    "youtube-recommendations": youtubeRecommendations,
+  } = settings;
+
+  const observerCallback = (mutations) => {
+    mutations.forEach((mutation) => {
+      if (mutation.type === "childList" || mutation.type === "attributes") {
+        if (youtubeAds !== false) {
+          try {
+            stopYoutubeAd();
+          } catch (e) {
+            if (youtubeErrors) {
+              const errorInfo = { stack: e.stack };
+
+              chrome.storage.sync.set({ "youtube-error-message": errorInfo });
+            }
+          }
+        }
+
+        if (youtubeShorts) {
+          try {
+            hideYoutubeShorts();
+          } catch (e) {
+            console.error(e);
+          }
+        }
+
+        if (youtubeRecommendations) {
+          try {
+            removeRecommendationsAtTheEnd();
+          } catch (e) {
+            console.error(e);
+          }
+        }
+      }
+    });
+  };
+
+  const observer = new MutationObserver(observerCallback);
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+    attributes: true,
+    attributeFilter: ["class"],
+  });
+
+  // Initial check in case elements are already present
+  if (youtubeAds !== false) {
+    stopYoutubeAd();
+  }
+  if (youtubeShorts) {
+    hideYoutubeShorts();
+  }
+  if (youtubeRecommendations) {
+    removeRecommendationsAtTheEnd();
   }
 };
 
-if (typeof window !== "undefined") {
-  chrome.storage.sync
-    .get([
-      "youtube-ads",
-      "youtube-shorts",
-      "youtube-errors",
-      "youtube-recommendations",
-    ])
-    .then(
-      ({
-        "youtube-ads": youtubeAds,
-        "youtube-shorts": youtubeShorts,
-        "youtube-errors": youtubeErrors,
-        "youtube-recommendations": youtubeRecommendations,
-      }) => {
-        new MutationObserver(() => {
-          if (youtubeAds === true || youtubeAds === undefined) {
-            try {
-              stopYoutubeAd();
-            } catch (e) {
-              if (youtubeErrors) {
-                // Extract relevant information from the error object
-                const errorInfo = {
-                  stack: e.stack,
-                };
-
-                // Save the error information to chrome storage
-                chrome.storage.sync.set({ "youtube-error-message": errorInfo });
-              }
-            }
-          }
-
-          if (youtubeShorts) {
-            try {
-              hideYoutubeShorts();
-            } catch (e) {
-              console.error(e);
-            }
-          }
-
-          if (youtubeRecommendations) {
-            try {
-              removeRecommendationsAtTheEnd();
-            } catch (e) {
-              console.error(e);
-            }
-          }
-        }).observe(document.querySelector(".html5-video-player"), {
-          attributes: true,
-        });
-      },
-    );
-}
+// Load user settings and start observers
+chrome.storage.sync
+  .get([
+    "youtube-ads",
+    "youtube-shorts",
+    "youtube-errors",
+    "youtube-recommendations",
+  ])
+  .then((settings) => {
+    startObservers(settings);
+  });
